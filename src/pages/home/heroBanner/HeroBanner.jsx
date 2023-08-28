4c2c0f3af6f37d49ecf5f76d85d048140ee244b4
DESKTOP-7GHR4AI\Maulin <maulinmakwana29@gmail.com>
2023-08-28 15:42:11 +0530

    Search button error fixed

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -28,6 +28,12 @@ const HeroBanner = () => {
     }
   };
 
+  const searchQuery = () => {
+    if (query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
   return (
     <div className="heroBanner">
       {!loading && (
@@ -50,7 +56,7 @@ const HeroBanner = () => {
               onChange={(e) => setQuery(e.target.value)}
               onKeyUp={searchQueryHandler}
             />
-            <button>Search</button>
+            <button onClick={searchQuery}>Search</button>
           </div>
         </div>
       </ContentWrapper>
